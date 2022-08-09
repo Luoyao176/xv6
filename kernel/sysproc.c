f6bffff8af2af1382a3da97ac342a40cb6a0ac03
Luoyao176 <1256786212@qq.com>
2022-08-09 15:37:34 +0800

    lazy

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -41,14 +41,35 @@ sys_wait(void)
 uint64
 sys_sbrk(void)
 {
-  int addr;
+/*  int addr;
   int n;
 
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+ // if(growproc(n) < 0)
+   // return -1;
+  myproc()->sz += n;
+  return addr;*/
+  int n;
+  if(argint(0, &n) < 0)
     return -1;
+  struct proc *p = myproc();
+  uint64 addr = p->sz;
+  uint64 newSize = addr + n;
+  if(newSize >= MAXVA)
+    return addr;
+  if(n<0)
+  {
+    if(newSize > addr)
+    {
+      newSize = 0;
+      uvmunmap(p->pagetable, 0, PGROUNDUP(addr)/PGSIZE, 1);
+    }
+    else
+      uvmunmap(p->pagetable, PGROUNDUP(newSize), (PGROUNDUP(addr) - PGROUNDUP(newSize))/PGSIZE, 1);
+  }
+  p->sz = newSize;
   return addr;
 }
 
